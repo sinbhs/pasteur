7b37771ae20f1206784f3f4857b6ef20b598df68
sinbhs <sinbhs@naver.com>
2020-10-25 23:59:11 +0900

    add Mobile Overlay Menu

--- js/index.js
+++ js/index.js
@@ -5,11 +5,22 @@ $(function(){
         // return false;
     });
 
+    //mobile menu 클릭시 overlay_menu 등장
+    $('.mobile_menu').on('click',function(){
+        $('.overlay_menu').slideDown().addClass('on');
+        $('.mask').addClass('on');
+    });
+    //mobile overlay_menu close_btn 클릭시 메뉴 닫기
+    $('.overlay_menu .close_btn').on('click',function(){
+        $('.overlay_menu').slideUp().removeClass('on');
+        $('.mask').removeClass('on');
+    });
+
     //scrollTop scroll event 스크롤 이벤트 - scrollTop버튼 등장
     $(window).scroll(function(){
         $('.scrollTop').stop().animate({opacity:0},700);
         var scroll = $(this).scrollTop();
-        if(scroll > 90) {
+        if(scroll > 400) {
             $('.scrollTop').stop().animate({opacity:1});
         }
     });
