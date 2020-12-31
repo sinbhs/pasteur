a38abab7547733e79f5210055a75b810abee9cb3
sinbhs <sinbhs@naver.com>
2020-12-31 18:22:42 +0900

    Add swiper-slide function

--- js/index.js
+++ js/index.js
@@ -31,4 +31,18 @@ $(function(){
             scrollTop:0
         },600);
     });
+
+    //subbn swiper
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 });
